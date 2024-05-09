33c916d171ff0a5525d6e4f03916c047aa22f687
Rahulkrishnan R <rahul.krishnan@talenq.com>
2024-05-09 09:58:31 +0530

    home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,34 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* IMAGE CONTAINER */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <Image src="/hero.png" alt="hero" fill className="object-contain" />
+      </div>
+      {/* TEXT CONTAINER */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+        {/* TITLE */}
+        <h1 className="text-4xl md:text-6xl font-bold">
+          Crafting Digital Experiences, Designing Tomorrow
+        </h1>
+        {/* DESCRIPTION */}
+        <p className="md:text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nam
+          aut rerum nostrum optio, debitis quas odio adipisci vel aliquam
+          repellat officiis asperiores sit eos dolor voluptas. Illum, beatae ad!
+        </p>
+        {/* BUTTONS */}
+        <div className="w-full flex gap-4">
+          <button className="p-4 rounded ring-1 ring-black bg-black text-white">
+            View My Work
+          </button>
+          <button className="p-4 rounded ring-1 ring-black">Contact Me</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
